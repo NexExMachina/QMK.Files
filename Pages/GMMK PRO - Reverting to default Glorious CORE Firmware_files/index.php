ad4ada365da3fcb81915a0e4f1b505167ed6d9dd
Nex <boultoncomputing@Gmail.com>
2024-01-28 21:00:33 +0000

    init

---
+++ Pages/GMMK PRO - Reverting to default Glorious CORE Firmware_files/index.php
@@ -0,0 +1,87 @@
+.Loading-image {
+  background:#fff url('/themes/kb/minimalist/img/loading.gif') no-repeat center center;
+}
+
+.SendFile.Uploading .Icon {
+  background:url('/themes/kb/minimalist/img/loading-small.gif') no-repeat;
+}
+.FileIcon {
+  background:url("/themes/kb/minimalist/img/file.png");
+}
+.InputFileView .DiscardButton {
+  background:url("/themes/kb/minimalist/img/file_discard.png") no-repeat center center;
+}
+.ErrorMessage .MessageWindowButtonClose {
+  background:url('/themes/kb/minimalist/img/popup_close.png') no-repeat 0 0px;
+}
+.ErrorMessage .MessageWindowButtonClose-over {
+  background:url('/themes/kb/minimalist/img/popup_close.png') no-repeat 0px -17px;
+}
+.CloseLabel {
+  background:url('/themes/kb/minimalist/img/popup_close.png') no-repeat 0 0px;
+}
+.CloseLabel:hover {
+  background:url('/themes/kb/minimalist/img/popup_close.png') no-repeat 0px -17px;
+}
+.AppBrandProductLink,
+.AppBrandProductLabel {
+  color:#000 !important;
+  opacity: 0.5;
+}
+.AppBrandProductLink:hover {
+  opacity: 0.8;
+}body {font-family:Arial,Verdana;}
+.MiniKbLoginBox {font-family:"Lucida grande","Lucida Sans Unicode",Arial,Verdana,Helvetica,Geneva,sans-serif;}
+.g-FormField2-ErrorPopup {font-family:"Lucida grande","Lucida Sans Unicode",Arial,Verdana,Helvetica,Geneva,sans-serif;}
+.MiniMessageFormFiles {font-family:Arial;}
+.MiniPreviewBox {font-family:Arial;}
+.MiniFileNameBox {font-family:Arial;}
+.MiniSingleContentPanel {font-family:"Lucida grande","Lucida sans unicode", Arial, Verdana, Helvetica, Geneva, sans-serif;}
+.MiniTime {font-family:Arial;}
+.MiniMessageGroupHeader {font-family:Arial;}
+.MiniMessageHeader .messageBody {font-family:Arial,"Lucida Sans Unicode",Verdana;}
+.inputViewContainer .MiniMessageFormFiles {font-family:Arial;}h1 {color:#333333}
+.CategoryTitleBox .CategoryLink {color:#474747;}
+.CategoryTitleBox .CategoryLink:hover {color:#474747;}
+.SearchResult .ArticleLink, .ArticleBox .ArticleLink, .SuggestionBox .ArticleLink {color:#474747;}
+.SearchResult .ArticleLink:hover, .ArticleBox .ArticleLink:hover, .SuggestionBox .ArticleLink:hover {color:#474747;}
+.ArticleReadMore {color:#474747;}
+.ArticleReadMore:hover {color:#474747;}
+
+#Header {background:#000000;}
+.KBLogo {background-image:url('//glorious.ladesk.com/scripts/file.php?view=Y&file=wxmeel045231f4s27ltahglj653dsvwb');}
+.RightBox {border-color:#EFEFEF;}
+.KbLinkButton {color:#FFFFFF; background-color: #FCA836; border-color:#D4943D;}
+.KbLinkButton:hover {color:#FFFFFF; background-color: #F19E2D; border-color:#D4943D;}
+.ImLeButton .buttonBorderColor, .UploadImageButton {border-color:#D3D3D3;}
+.ImLeButton .buttonBgColor, .UploadImageButton {background-color:#FFFFFF;}
+.ImLeButton .buttonOverBorderColor, .UploadImageButton:hover {border-color:#C3C3C3;}
+.ImLeButton .buttonOverBgColor, .UploadImageButton:hover {background-color:#F5F5F5;}
+.ImLeButton .buttonText, .UploadImageButton .Text {color:#000000;}
+.ImLeButton-over .buttonText, .UploadImageButton:hover .Text {color:#000000;}
+
+.MainButton .buttonBorderColor, .GreenButton .buttonBorderColor, .Send .buttonBorderColor, .ReplyButton .buttonBorderColor {border-color:#D4943D;}
+.MainButton .buttonBgColor, .GreenButton .buttonBgColor, .Send .buttonBgColor, .ReplyButton .buttonBgColor {background-color:#FCA836;}
+.MainButton .buttonOverBorderColor, .GreenButton .buttonOverBorderColor, .Send .buttonOverBorderColor, .ReplyButton .buttonOverBorderColor {border-color:#D4943D;}
+.MainButton .buttonOverBgColor, .GreenButton .buttonOverBgColor, .Send .buttonOverBgColor, .ReplyButton .buttonOverBgColor {background-color:#F19E2D;}
+.MainButton .buttonText, .GreenButton .buttonText, .Send .buttonText, .ReplyButton .buttonText {color:#FFFFFF;}
+.MainButton .buttonOverBgColor .buttonText, .GreenButton .buttonOverBgColor .buttonText, .Send .buttonOverBgColor .buttonText, .ReplyButton .buttonOverBgColor .buttonText {color:#FFFFFF;}
+
+.JumpTo {color:#FCA836;}
+.JumpTo:before {border-left-color:#FCA836;}
+.CategoryBox .ArticleBoxIcon {background-color:#FCA836; border-color:#FCA836;}
+.CategoryBoxQ .ArticleBoxIcon {background-color:transparent; border-color:#FCA836;}
+  .CategoryBoxG .ArticleBoxIcon {background-color:transparent; border-color:#FCA836; border-width:3px;}
+.CategoryLink .SmallIcon {background-color:#FCA836; border-color:#FCA836;}
+  .CategoryLink.CategoryLinkG .SmallIcon {background-color:transparent; border-color:#FCA836; border-width:2px;}
+  .CategoryLink.CategoryLinkQ .SmallIcon {background-color:transparent; border-color:#FCA836;}
+.ArticleLink .SmallIcon {background-color:#000000; border-color:#CECECE;}
+.ItemLink .SmallIcon {background-color:#000000; border-color:#CECECE;}
+  .ItemLinkG .SmallIcon {background-color:transparent; border-color:#FCA836; border-width:2px;}
+  .ItemLinkQ .SmallIcon {background-color:transparent; border-color:#FCA836;}
+  .ItemLinkC .SmallIcon {background-color:#FCA836; border-color:#FCA836;}
+  .ItemLinkR .SmallIcon {background-color:transparent; border-color:#CECECE;}
+  .ItemLinkS .SmallIcon {background-color:transparent; border-color:#CECECE; border-width:2px;}
+.SearchResult .ArticleBoxIcon, .ArticleBox .ArticleBoxIcon, .SuggestionBox .ArticleBoxIcon {background-color:#000000; border-color:#CECECE;}
+  .ArticleBox.Topic .ArticleBoxIcon {background-color:transparent; border-color:#CECECE;}
+  .ArticleBox.Suggestion .ArticleBoxIcon {background-color:transparent; border-color:#CECECE; border-width:2px;}
